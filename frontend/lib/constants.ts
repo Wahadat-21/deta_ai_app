5a44853d19a0c1438403f185cf649974b777c3e1
Syed Sian Wahadat <sianwahadat23@gmail.com>
2026-07-18 00:09:56 +0600

    Create constants.ts

---
+++ frontend/lib/constants.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = "DentalAI";
+
+export const API_VERSION = "v1";
+
+export const MAX_UPLOAD_SIZE = 10;
+
+export const ACCEPTED_IMAGES = [
+  "image/jpeg",
+  "image/png",
+  "image/webp"
+];
